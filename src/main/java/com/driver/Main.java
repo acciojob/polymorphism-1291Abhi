baef2c6e450d6952b7174432214b3814e5691935
Abhinesh Singh <abhisingh1291@gmail.com>
2023-04-14 14:37:43 +0530

    Product class created and overloading of product method is implemented

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,24 @@
 package com.driver;
 
 public class Main {
+    static class Product{
+        public  int product(int x,int y){
+            return x*y;
+        }
+        public int product(int x,int y,int z){
+            return x*y*z;
+        }
+        public  double product(double x,double y){
+            return x*y;
+        }
+
+    }
+    public static void main(String args[]){
+        Product p=new Product();
+        int ans1=p.product(5,4);
+        int ans2=p.product(5,6,7);
+        double ans3=p.product(5.0,6.8);
+        System.out.println(ans1+" "+ans2+" "+ans3);
+    }
 
 }
